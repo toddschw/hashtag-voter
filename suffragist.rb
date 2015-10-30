13d327df93fc1fc86e24628deaa7bcf7bda94f05
Todd Schwartzfarb <todd@wirestreak.com>
2015-10-29 20:11:03 -0400

    updated files

--- suffragist.rb
+++ suffragist.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
+require 'yaml/store'
+
+
 
 Choices = {
-  'tag1' => '#wyn #1',
-  'tag2' => '#wyn #2',
-  'tag3' => '#wyn #3'
+  'tag1' => '#wyndough',
+  'tag2' => '#damons_lil_demons',
 }
 
 get '/' do
@@ -14,11 +16,36 @@ end
 post '/cast' do
   @title = 'Thanks for casting your vote'
   @vote = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
   erb :cast
 end
 
 get '/results' do
-  @title = "Here are the results:"
-  @votes = { 'tag1' => 7, 'tag2' => 5, 'tag3' => 3 }
+  @title = "Here are the results so far:"
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes']}
   erb :results
+end
+
+get '/suggest' do
+  @title = "Suggest a new hashtag"
+  erb :suggest
+end
+
+post '/suggest' do
+  @title = 'Your suggestion is submitted'
+  @suggestion = params["hashtag"]
+  l = Choices.length
+  new_tag = "tag #{l}"
+  Choices["#{new_tag}"] = @suggestion
+  erb :gotit
+end
+
+get '/votes.yml' do
+  ??
 end
